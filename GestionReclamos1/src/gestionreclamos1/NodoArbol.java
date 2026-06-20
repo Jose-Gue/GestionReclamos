951ba309e0cab649a1dff6e617a7efcd4fad8899
Jose-Gue <joseignacio3w1@gmail.com>
2026-06-20 12:53:47 -0400

    Clase NodoArbol para el arbol binario de busqueda

---
+++ GestionReclamos1/src/gestionreclamos1/NodoArbol.java
@@ -0,0 +1,46 @@
+package gestionreclamos1;
+
+/**
+ *
+ * @author salaz
+ */
+public class NodoArbol {
+    
+    private Reclamo reclamo;
+    private NodoArbol izquierda;
+    private NodoArbol derecha;
+    
+    public NodoArbol(Reclamo reclamo) {
+        this.reclamo = reclamo;
+        this.izquierda = null;
+        this.derecha = null;
+    }
+
+    public Reclamo getReclamo() {
+        return reclamo;
+    }
+
+    public void setReclamo(Reclamo reclamo) {
+        this.reclamo = reclamo;
+    }
+
+    public NodoArbol getIzquierda() {
+        return izquierda;
+    }
+
+    public void setIzquierda(NodoArbol izquierda) {
+        this.izquierda = izquierda;
+    }
+
+    public NodoArbol getDerecha() {
+        return derecha;
+    }
+
+    public void setDerecha(NodoArbol derecha) {
+        this.derecha = derecha;
+    }
+    
+    
+    
+    
+}
